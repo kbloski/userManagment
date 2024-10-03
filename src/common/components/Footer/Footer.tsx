271a9f0d47cf04c8744b495b626a3a5dedb5a2a5
Kamil Błoński <kblonski02@gmail.com>
2024-10-03 23:05:46 +0200

    add footer style and header style

--- src/common/components/Footer/Footer.tsx
+++ src/common/components/Footer/Footer.tsx
@@ -1,5 +1,8 @@
+import style from './footer.module.css';
+
+
 export function Footer(){
-    return <footer>
-        Footer
+    return <footer className={style.footer}>
+        Dziękuję za skorzystanie z mojej aplikacji
     </footer>
 }
--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -2,9 +2,7 @@ import style from './header.module.css';
 import { Navigation } from "../Navigation/Navigation";
 
 export function Header(){
-    // return <header className={style.header}>
       return  <header className={style.header}>
             <Navigation />
         </header>
-    {/* </header> */}
 }
