a0abe849b2a899c68c878d2142a0faf20c6623db
Kamil Błoński <kblonski02@gmail.com>
2024-10-03 13:39:47 +0200

    Add css style to login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,26 +1,30 @@
 'use client';
-// import style from './login.module.css';
+import style from './login.module.css';
 // import { useState } from "react";
 
 
-export default function LoginPage(){
+export default function LoginPage() {
     // const user = useState();
 
-    return(
-        <>
-            <div className='container'>
-                <form>
-                    <div>
-                        <input type="text" id="username" placeholder=" "/>
-                        <label htmlFor="username">Username</label>
+    return (
+            <div className={style.container}>
+                <div className={style.bg}>
+                    <div className={style['bg-rect']}></div>
+                    <div className={style['bg-rect']}></div>
+                </div>
+                <form className={style.form}>
+                    <div className={style['form-icon']}></div>
+                    <div className={style['input-container']}>
+                        <input type="text" id="username" placeholder=" " className={style['form-input']} />
+                        <label htmlFor="username" className={style['form-label']}>Username</label>
                     </div>
-                    <div>
-                        <input type="password" id="password" placeholder=" "/>
-                        <label htmlFor="password">Username</label>
+                    <div className={style['input-container']}>
+                        <input type="password" id="password" placeholder=" " className={style['form-input']}/>
+                        <label htmlFor="password" className={style['form-label']}>Password</label>
                     </div>
-                    <button type='submit'>Login</button>
+                    <div className={style['form-spacer']}></div>
+                    <button type='submit' className={style['button-submit']}>Login</button>
                 </form>
             </div>
-        </>
-    ) 
+    )
 }
