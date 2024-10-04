3273a085382e13030d5d912f8da6504977425deb
Kamil Błoński <kblonski02@gmail.com>
2024-10-04 02:31:39 +0200

    update styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,16 @@ import { UserList } from "@/common/components/UserList/UserList";
 export default function Home() {
   return (
     <div className={styles.container}>
-        <h1>App: Resource-Allowed-By-User-Role</h1>
+        <h1>Zarządzanie Stronami przez Role Użytkowników</h1>
         <p>
-          Wykorzystano: Html, Css, Ts, NextJs(AppRouter), CssModule, Html, Client and React Components, NextApi
+          Wykorzystano: Html, Css, Ts, NextJs(AppRouter), CssModule, Html, Client and React Components, NextApi, LocalStorage
         </p>
-        
-        Dostępni użytkownicy:
+        <h2>
+          Opis strony
+        </h2>
+        <p>
+        Strona umożliwia zarządzanie użytkownikami w oparciu o przypisane im role. Użytkownicy mający określone uprawnienia mogą korzystać z pełnej funkcjonalności serwisu, natomiast użytkownicy o niższych rolach są ograniczeni w dostępie do niektórych treści. W przypadku, gdy użytkownik próbuje odwiedzić adres URL, do którego nie ma uprawnień, zostaje automatycznie przekierowany na stronę główną, co zapewnia bezpieczeństwo i właściwą organizację dostępu do zasobów.
+        </p>
         <UserList />
               
     </div>
--- src/common/components/UserList/UserList.tsx
+++ src/common/components/UserList/UserList.tsx
@@ -1,23 +1,29 @@
+import style from './userList.module.css';
 import { userList } from "@/app/api/user/userList"
 import { UserRoles } from "@/common/types/UserType"
 
 export function UserList(){
 
     return <div>
-        <table>
+        <table className={style.table}>
             <tr>
+                <th colSpan={4}>
+                    Lista dostępnych użytkowników
+                </th>
+            </tr>
+            <tr className={style.tr}>
                 <th>ID</th>
                 <th>Username</th>
-                <th>Role</th>
                 <th>Password</th>
+                <th>Role</th>
             </tr>
             {
             userList.map( user => {
-                return <tr key={user.id}>
-                <td>{user.id}</td>
-                <td>{user.username}</td>
-                <td>{UserRoles[user.role]}</td>
-                <td>{user.password}</td>
+                return <tr key={user.id} className={style.tr}>
+                    <td>{user.id}</td>
+                    <td>{user.username}</td>
+                    <td>{user.password}</td>
+                    <td>{UserRoles[user.role]}</td>
                 </tr>
             })
             }
